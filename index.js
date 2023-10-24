1abec7e367497fa475bdb06f3df06408502dfd47
Djair Alves <djair.dj@gmail.com>
2023-10-24 13:22:39 -0300

    espaços com excesso removidos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//O arquivo principal que inicia a aplicação. 
-//Ele importa os módulos necessários, cria a 
+//O arquivo principal que inicia a aplicação.
+//Ele importa os módulos necessários, cria a
 //instância da aplicação e inicia a interação com o usuário.
 
 import { startApp } from "./src/cli.js";
