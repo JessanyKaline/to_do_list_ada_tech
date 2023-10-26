fb831cc6e6b214fed2f138cdd62002c4ee8f882d
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2023-10-26 20:19:28 -0300

    changed getTasks user exibition

--- toDoList.js
+++ toDoList.js
@@ -143,8 +143,9 @@ function newTask(taskList) {
 
 function getTasks(taskList) {
     try {
-        for (const task of taskList) {
-            console.log(task);
+        console.log("\nLista de Tarefas:")
+        for (let i = 0; i < taskList.length; i++) {
+            console.log(`${(i + 1)} - ${taskList[i].title}:1\n\t${taskList[i].description}`)
         }
     } catch (error) {
         console.error('Ocorreu um erro inesperado ao exibir as tarefas:', error.message);
@@ -181,7 +182,7 @@ function updateTask(taskList) {
 }
 
 function enumerateTask(taskList) {
-    console.log("Lista de Tarefas: \n")
+    console.log("\nLista de Tarefas:")
     for (let i = 0; i < taskList.length; i++) {
         console.log(`${(i + 1)} - ${taskList[i].title}`)
     }
